7d8a8070496875190715a067b774cb4081f69f54
ramibakir <ramiibakiir@gmail.com>
2020-09-04 14:56:26 +0200

    Created jsCarido files
    Removed onclick on buttons in todoOveriew and added autocomplete=off for checkbox.
    Added a lot of code in todoScript

--- todoScript.js
+++ todoScript.js
@@ -1,6 +1,17 @@
 let todoTitles = [];
 let todoDescriptions = [];
 let todoAuthor = [];
+let todoDate = [];
+
+let todoOneTitle = document.getElementById("todoOneTitle").innerHTML;
+let todoOneDescription = document.getElementById("todoOneDescription").innerHTML;
+let todoTwoTitle = document.getElementById("todoTwoTitle").innerHTML;
+let todoTwoDescription = document.getElementById("todoTwoDescription").innerHTML;
+let todoThreeTitle = document.getElementById("todoThreeTitle").innerHTML;
+let todothreeDescription = document.getElementById("todoThreeDescription").innerHTML;
+
+todoTitles = [todoOneTitle, todoTwoTitle, todoThreeTitle];
+todoDescriptions = [todoOneDescription, todoTwoDescription, todothreeDescription];
 
 function overlayOn() {
     document.getElementById("overlay").style.display = "block";
@@ -9,78 +20,6 @@ function overlayOn() {
 
 function overlayOff() {
     document.getElementById("overlay").style.display = "none";
-}
-
-function getCurrentTodos() {
-    /* Fill arrays with HTML */
-    let todoOneTitle = document.getElementById("todoOneTitle").innerHTML;
-    let todoOneDescription = document.getElementById("todoOneDescription").innerHTML;
-    let todoTwoTitle = document.getElementById("todoTwoTitle").innerHTML;
-    let todoTwoDescription = document.getElementById("todoTwoDescription").innerHTML;
-    let todoThreeTitle = document.getElementById("todoThreeTitle").innerHTML;
-    let todothreeDescription = document.getElementById("todoThreeDescription").innerHTML;
-
-    /* Arrays */
-    todoTitles = [todoOneTitle, todoTwoTitle, todoThreeTitle];
-    todoDescriptions = [todoOneDescription, todoTwoDescription, todothreeDescription];
-}
-
-function deleteTodoOne() {
-    const delBtnOne = document.getElementById("btn-delTodo1").addEventListener("click", deleteTodoOne);
-        document.getElementById("todoOneTitle").innerHTML = "";
-        document.getElementById("todoOneDescription").innerHTML = "";
-}
-
-function deleteTodoTwo() {
-    const delBtnTwo = document.getElementById("btn-delTodo2").addEventListener("click", deleteTodoTwo);
-        document.getElementById("todoTwoTitle").innerHTML = "";
-        document.getElementById("todoTwoDescription").innerHTML = "";
-}
-
-function deleteTodoThree() {
-    const delBtnThree = document.getElementById("btn-delTodo3").addEventListener("click", deleteTodoThree);
-        document.getElementById("todoThreeTitle").innerHTML = "";
-        document.getElementById("todoThreeDescription").innerHTML = "";
-}
-
-function completeTodoOne(){
-    const cmpBtnOne = document.getElementById("btn-cmpTodo1").addEventListener("click", completeTodoOne);
-    let completedTitle = document.getElementById("todoOneTitle").innerHTML;
-    let completedDescription = document.getElementById("todoOneDescription").innerHTML;
-    let tableContentTitle = document.getElementById("titCL1").innerHTML = completedTitle;
-    let tableContentDescription = document.getElementById("descCL1").innerHTML = completedDescription;
-    
-    document.getElementById("todoOneTitle").innerHTML = "";
-    document.getElementById("todoOneDescription").innerHTML = "";
-}
-
-function completeTodoTwo() {
-    const cmpBtnTwo = document.getElementById("btn-cmpTodo2").addEventListener("click", completeTodoTwo);
-
-    let completedTitle = document.getElementById("todoTwoTitle").innerHTML;
-    let completedDescription = document.getElementById("todoTwoDescription").innerHTML;
-    let tableContentTitle = document.getElementById("titCL2").innerHTML = completedTitle;
-    let tableContentDescription = document.getElementById("descCL2").innerHTML = completedDescription;
-    
-    document.getElementById("todoTwoTitle").innerHTML = "";
-    document.getElementById("todoTwoDescription").innerHTML = "";
-}
-
-function completeTodoThree() {
-    const cmpBtnThree = document.getElementById("btn-cmpTodo3").addEventListener("click", completeTodoThree);
-
-    let completedTitle = document.getElementById("todoThreeTitle").innerHTML;
-    let completedDescription = document.getElementById("todoThreeDescription").innerHTML;
-    let tableContentTitle = document.getElementById("titCL3").innerHTML = completedTitle;
-    let tableContentDescription = document.getElementById("descCL3").innerHTML = completedDescription;
-    
-    document.getElementById("todoThreeTitle").innerHTML = "";
-    document.getElementById("todoThreeDescription").innerHTML = "";
-}
-
-function countDown() {
-    document.getElementById("charactersLeft").innerHTML = 125 -
-        document.getElementById("description").value.length;
 }
 
 function createTodo() {
@@ -92,20 +31,109 @@ function createTodo() {
     todoDescriptions.push(description);
     todoAuthor.push(author);
 
-    const currentTitleOne = document.getElementById("todoOneTitle").innerHTML; 
+    const currentTitleOne = document.getElementById("todoOneTitle").innerHTML;
     const currentDescriptionOne = document.getElementById("todoOneDescription").innerHTML;
 
     document.getElementById("todoOneTitle").innerHTML = title;
     document.getElementById("todoOneDescription").innerHTML = description;
 
-    if(currentTitleOne != title && currentDescriptionOne != description){
+    if (currentTitleOne != title && currentDescriptionOne != description) {
         document.getElementById("titCL1").innerHTML = currentTitleOne;
         document.getElementById("descCL1").innerHTML = currentDescriptionOne;
         document.getElementById("autCL1").innerHTML = "Rami Bakir";
         document.getElementById("dateCL1").innerHTML = "05.01.21";
     }
-
-
     document.getElementById("overlay").style.display = "none";
-
 }
+
+function deleteTodoOne() {
+    const delBtnOne = document.getElementById("btn-delTodo1").addEventListener("click", deleteTodoOne);
+    document.getElementById("todoOneTitle").innerHTML = "";
+    document.getElementById("todoOneDescription").innerHTML = "";
+}
+
+function deleteTodoTwo() {
+    const delBtnTwo = document.getElementById("btn-delTodo2").addEventListener("click", deleteTodoTwo);
+    document.getElementById("todoTwoTitle").innerHTML = "";
+    document.getElementById("todoTwoDescription").innerHTML = "";
+}
+
+function deleteTodoThree() {
+    const delBtnThree = document.getElementById("btn-delTodo3").addEventListener("click", deleteTodoThree);
+    document.getElementById("todoThreeTitle").innerHTML = "";
+    document.getElementById("todoThreeDescription").innerHTML = "";
+}
+
+document.getElementById("btn-cmpTodo1").addEventListener("click", event => {
+    let dateObject = new Date();
+    let today = dateObject.getDate() + '.' + (dateObject.getMonth() + 1) + '.' + dateObject.getFullYear();
+
+    for (let titles = 0; titles < todoTitles.length; titles++) {
+        if (todoTitles[titles] == todoOneTitle) {
+            document.getElementById("titCL1").innerHTML = todoTitles[titles];
+        }
+    }
+
+    for (let descriptions = 0; descriptions < todoDescriptions.length; descriptions++) {
+        if (todoDescriptions[descriptions] == todoOneDescription){
+            document.getElementById("descCL1").innerHTML = todoDescriptions[descriptions];
+        }
+    }
+
+
+    document.getElementById("dateCL1").innerHTML = today.toString();
+    todoDate.push(document.getElementById("dateCL1").innerHTML);
+
+    document.getElementById("todoOneTitle").innerHTML = "";
+    document.getElementById("todoOneDescription").innerHTML = "";
+    console.log("please");
+
+});
+
+document.getElementById("btn-cmpTodo2").addEventListener("click", event => {
+    let dateObject = new Date("2020-08-28");
+    let today = dateObject.getDate() + '.' + (dateObject.getMonth() + 1) + '.' + dateObject.getFullYear();
+
+    document.getElementById("titCL2").innerHTML = document.getElementById("todoTwoTitle").innerHTML;
+    document.getElementById("descCL2").innerHTML = document.getElementById("todoTwoDescription").innerHTML;
+
+    document.getElementById("dateCL2").innerHTML = today.toString();
+    todoDate.push(document.getElementById("dateCL2").innerHTML);
+
+    document.getElementById("todoTwoTitle").innerHTML = "";
+    document.getElementById("todoTwoDescription").innerHTML = "";
+    console.log("??????");
+});
+
+document.getElementById("btn-cmpTodo3").addEventListener("click", event => {
+    let dateObject = new Date('2020-09-01');
+    let today = dateObject.getDate() + '.' + (dateObject.getMonth() + 1) + '.' + dateObject.getFullYear();
+
+    document.getElementById("titCL3").innerHTML = document.getElementById("todoThreeTitle").innerHTML;
+    document.getElementById("descCL3").innerHTML = document.getElementById("todoThreeDescription").innerHTML;
+
+    document.getElementById("dateCL3").innerHTML = today.toString();
+    todoDate.push(document.getElementById("dateCL3").innerHTML);
+
+    document.getElementById("todoThreeTitle").innerHTML = "";
+    document.getElementById("todoThreeDescription").innerHTML = "";
+    console.log("FAEN");
+});
+
+function countDown() {
+    document.getElementById("charactersLeft").innerHTML = 125 -
+        document.getElementById("description").value.length;
+}
+
+document.getElementById("dateSorting").addEventListener("change", event => {
+    console.log(todoDate + " unsorted");
+    if(document.getElementById("dateSorting").checked){
+        for (let date = 0; date < todoDate.length; date++) {
+           if (todo) {
+               
+           }
+            console.log(todoDate + " was sorted");
+        }
+        //console.log(todoDate + " was sorted");
+    }
+});
